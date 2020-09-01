c89dfc44e516e9fd54321d62c7387690b3525544
Falet <miha.m1@yandex.ru>
2020-09-01 11:43:25 +0700

    Create MoveAsteroid.cs

---
+++ Numbers/Assets/Script/Asteroid/MoveAsteroid.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MoveAsteroid : MonoBehaviour
+{
+    [Range(0.0f,7.0f)]
+    public float Speed = 2.0f;
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        MovementLogic();
+    }
+    private void MovementLogic()
+    {
+        Vector3 movement = new Vector3(1.0f, 0.0f, 0.0f);
+        transform.Translate(movement * Speed * Time.fixedDeltaTime);
+    }
+}
