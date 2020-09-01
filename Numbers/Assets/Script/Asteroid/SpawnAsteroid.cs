b3facea3c1b48e2819a66b6ea6044a3619904e78
Falet <miha.m1@yandex.ru>
2020-09-01 11:43:29 +0700

    Create SpawnAsteroid.cs

---
+++ Numbers/Assets/Script/Asteroid/SpawnAsteroid.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnAsteroid : MonoBehaviour
+{
+    public int CountAsteroid = 5;
+    public List<GameObject> _Asteroid = new List<GameObject>();
+    public float _incrementSpeedAsteroid;
+
+    private int _counterForSpawn = 0;
+    private float xr;
+    private float yt;
+    private float yb;
+    private Vector3 _startPosition;
+    private Quaternion _rotationAsteroid;
+    private MoveAsteroid _changeSpeed;
+    private Transform _startTrans;
+    
+    private void Start()
+    {
+        Debug.Log(transform.localPosition.x);
+        Debug.Log(transform.lossyScale.x);
+        xr = transform.localPosition.x + transform.localScale.x;
+        yt = transform.localPosition.y + transform.localScale.y;
+        yb = transform.localPosition.y - transform.localScale.y;
+        
+        Debug.Log(yt);
+        _startPosition = new Vector3(xr, yb, 1.0f);
+        _startTrans.rotation = Quaternion.Euler(0, 0, 0);
+        _startTrans.position = _startPosition;
+        _startTrans.localScale = _Asteroid[_Asteroid.Count - 1].transform.localScale;
+        //_startPosition = new Vector3(Random.Range(yb, yt), xr, 1.0f);
+    }
+
+    private void Update()
+    {
+        if(_counterForSpawn < CountAsteroid)
+        {
+            //_startPosition.y = Random.Range(yt, yb);
+            if(_Asteroid.Count == 0)
+            {
+                GameObject Asteroid = CreateAsteroid();
+                Asteroid.GetComponent<CircleCollider2D>().radius += 1.5f;
+                
+            }
+            else
+            {
+                CreateAsteroid();
+                //if()
+            }
+            
+            _counterForSpawn++;
+        }
+    }
+    private GameObject CreateAsteroid()
+    {
+        GameObject _objAsteroid = Instantiate(_Asteroid[Random.Range(0, _Asteroid.Count - 1)], _startTrans);
+        _changeSpeed = _objAsteroid.GetComponent<MoveAsteroid>();
+        _changeSpeed.Speed = 2.0f + _incrementSpeedAsteroid;
+        return _objAsteroid;
+    }
+    public void Respawn(GameObject Asteroid)
+    {
+        _changeSpeed.Speed = Random.Range(0.07f, 7.0f);
+        _startPosition.y = Random.Range(yt, yb);
+        Asteroid.transform.position = _startPosition;
+    }
+
+}
