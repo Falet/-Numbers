ba238c4ce8c843263abd51b670e409dd494da0b2
Falet <miha.m1@yandex.ru>
2020-09-01 11:43:57 +0700

    Create DeleteButton.cs

---
+++ Numbers/Assets/Script/Numbers/DeleteButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class DeleteButton : MonoBehaviour, IPointerClickHandler
+{
+    public UnityEvent HitEvent;
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        HitEvent.Invoke();
+    }
+}
