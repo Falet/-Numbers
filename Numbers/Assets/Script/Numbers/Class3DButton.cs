81030c2166e36dd235524f4e86b0aff7eccbdeda
Falet <miha.m1@yandex.ru>
2020-09-01 11:43:47 +0700

    Create Class3DButton.cs

---
+++ Numbers/Assets/Script/Numbers/Class3DButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public abstract class Class3DButton<T> : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField]
+    protected UnityEvent<GameObject> _hitEventForPlayerContr;//Эвент для движения игрока
+    [SerializeField]
+    protected UnityEvent<T> _hitEventForMechanic;//Эвент для счета результата
+    [SerializeField]
+    protected UnityEvent<GameObject> _hitEventForCreatorText;//Эвент для создания объектов цифр
+    public GameObject TextNumOrSignOnScene;
+    public T WhatButton;
+    private bool _isLocked = false;
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        if(!_isLocked)
+        {
+            _hitEventForPlayerContr.Invoke(pointerEventData.pointerPress);
+            _hitEventForMechanic.Invoke(WhatButton);
+            _hitEventForCreatorText.Invoke(TextNumOrSignOnScene);
+        }
+        else
+        {
+
+        }
+    }
+}
