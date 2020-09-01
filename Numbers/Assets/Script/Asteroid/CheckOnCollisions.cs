d5ad6ea2b4e1d2d94e17d7446bc0d31330be8880
Falet <miha.m1@yandex.ru>
2020-09-01 11:43:21 +0700

    Create CheckOnCollisions.cs

---
+++ Numbers/Assets/Script/Asteroid/CheckOnCollisions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class CheckOnCollisions : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent _eventCollisionAsteroid;
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        _eventCollisionAsteroid.Invoke();
+    }
+}
