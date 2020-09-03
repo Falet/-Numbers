a4412f686a6c4e5b24b5531e575dfee96fc5cd9d
Falet <miha.m1@yandex.ru>
2020-09-03 16:11:47 +0700

    03/09/2020 16:11

--- Numbers/Assets/Script/Asteroid/SpawnAsteroid.cs
+++ Numbers/Assets/Script/Asteroid/SpawnAsteroid.cs
@@ -12,58 +12,50 @@ public class SpawnAsteroid : MonoBehaviour
     private float yt;
     private float yb;
     private Vector3 _startPosition;
-    private Quaternion _rotationAsteroid;
-    private MoveAsteroid _changeSpeed;
-    private Transform _startTrans;
-    
+    private Quaternion _startRotation;
+    private struct ObjAndSpeed
+    {
+        public GameObject Asteroid;
+        public MoveAsteroid SpeedAsteroid;
+        public ObjAndSpeed(GameObject Asteroid, MoveAsteroid SpeedAsteroid)
+        {
+            this.Asteroid = Asteroid;
+            this.SpeedAsteroid = SpeedAsteroid;
+        }
+        
+    }
+    private List<ObjAndSpeed> _arrayAsteroid = new List<ObjAndSpeed>();
     private void Start()
     {
-        //Debug.Log(transform.localPosition.x);
-        //Debug.Log(transform.lossyScale.x);
-        /*xr = transform.localPosition.x + transform.localScale.x;
-        yt = transform.localPosition.y + transform.localScale.y;
-        yb = transform.localPosition.y - transform.localScale.y;
-        
-        //Debug.Log(yt);
-        _startPosition = new Vector3(xr, yb, 1.0f);
-        _startTrans.rotation = Quaternion.Euler(0, 0, 0);
-        _startTrans.position = _startPosition;*/
         RectTransform _changeTransform = transform as RectTransform;
-        Debug.Log(_changeTransform.sizeDelta);
-        //_startTrans.localScale = _Asteroid[_Asteroid.Count - 1].transform.localScale;
-        //_startPosition = new Vector3(Random.Range(yb, yt), xr, 1.0f);
+
+        xr = _changeTransform.position.x + _changeTransform.sizeDelta.x / 2;
+        yt = _changeTransform.position.y + _changeTransform.sizeDelta.y / 2;
+        yb = _changeTransform.position.y - _changeTransform.sizeDelta.y / 2;
+
+        _startPosition = new Vector3(xr, yt, 1.0f);
+        _startRotation = Quaternion.Euler(0,0,0);
+
+        for(int i = 0;i < CountAsteroid;i++)
+        {
+            _startPosition.y = Random.Range(yb, yt);
+            GameObject ObjAsteroid = Instantiate(_prefubAsteroid[Random.Range(0, _prefubAsteroid.Count - 1)], _startPosition, _startRotation);
+            ObjAndSpeed ObjAndHisChangeSpeed = new ObjAndSpeed(ObjAsteroid, ObjAsteroid.GetComponent<MoveAsteroid>());
+            _arrayAsteroid.Add(ObjAndHisChangeSpeed);
+            ObjAsteroid.GetComponent<MoveAsteroid>().Speed = 2.0f + _incrementSpeedAsteroid;
+            _counterForSpawn++;
+        }
     }
 
     private void Update()
     {
-        /*if(_counterForSpawn < CountAsteroid)
-        {
-            //_startPosition.y = Random.Range(yt, yb);
-            if(_prefubAsteroid.Count == 0)
-            {
-                GameObject Asteroid = CreateAsteroid();
-                Asteroid.GetComponent<CircleCollider2D>().radius += 1.5f;
 
-            }
-            else
-            {
-                CreateAsteroid();
-                //if()
-            }
-            _counterForSpawn++;
-        }*/
-    }
-    private GameObject CreateAsteroid()
-    {
-        GameObject _objAsteroid = Instantiate(_prefubAsteroid[Random.Range(0, _prefubAsteroid.Count - 1)], _startTrans);
-        _changeSpeed = _objAsteroid.GetComponent<MoveAsteroid>();
-        _changeSpeed.Speed = 2.0f + _incrementSpeedAsteroid;
-        return _objAsteroid;
     }
     public void Respawn(GameObject Asteroid)
     {
-        _changeSpeed.Speed = Random.Range(0.07f, 7.0f);
-        _startPosition.y = Random.Range(yt, yb);
+        Asteroid.GetComponent<MoveAsteroid>().Speed = Random.Range(0.07f, 7.0f);
+        Asteroid.GetComponent<SpriteRenderer>().sprite = _prefubAsteroid[Random.Range(0, _prefubAsteroid.Count - 1)].GetComponent<SpriteRenderer>().sprite;
+        _startPosition.y = Random.Range(yb, yt);
         Asteroid.transform.position = _startPosition;
     }
 
