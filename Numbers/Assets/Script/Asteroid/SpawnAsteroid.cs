a3a8bb226dd5705bf947824c11d91f170739bc12
Falet <miha.m1@yandex.ru>
2020-09-06 16:35:26 +0700

    06.09.2020

--- Numbers/Assets/Script/Asteroid/SpawnAsteroid.cs
+++ Numbers/Assets/Script/Asteroid/SpawnAsteroid.cs
@@ -10,10 +10,12 @@ public class SpawnAsteroid : MonoBehaviour
     private int _counterForSpawn = 0;
     private CheckOnBarrier _checkotheasteroid;
     private Vector3 _startPosition;
-    private Vector2 StartpositionRayUpper;
-    private Vector2 StartpositionRayLower;
     private Quaternion _startRotation;
     private Dictionary<GameObject, Asteroid> _attachedScriptsToObj = new Dictionary<GameObject, Asteroid>();
+    public Dictionary<GameObject, Asteroid> GetAttachedScriptsToObj()
+    {
+        return _attachedScriptsToObj;
+    }
     /*private struct ObjAndSpeed
     {
         public GameObject Asteroid;
@@ -28,18 +30,18 @@ public class SpawnAsteroid : MonoBehaviour
         
     }*/
     //private List<Asteroid> _arrayAsteroid = new List<Asteroid>();
-    private FindExtremePoints ValueExtremePoints;
+    private FindExtremePoints _valueExtremePoints;
     private void Start()
     {
-        ValueExtremePoints = GetComponent<FindExtremePoints>();
+        _valueExtremePoints = GetComponent<FindExtremePoints>();
         _checkotheasteroid = GetComponent<CheckOnBarrier>();
 
-        _startPosition = new Vector3(ValueExtremePoints.XRight + 10.0f, ValueExtremePoints.YTop, 1.0f);
+        _startPosition = new Vector3(_valueExtremePoints.XRight + 10.0f, _valueExtremePoints.YTop, 1.0f);
         _startRotation = Quaternion.Euler(0,0,0);
         
         for (_counterForSpawn = 0; _counterForSpawn < CountAsteroid; _counterForSpawn++)
         {
-            _startPosition.y = Random.Range(ValueExtremePoints.YBot, ValueExtremePoints.YTop);
+            _startPosition.y = Random.Range(_valueExtremePoints.YBot, _valueExtremePoints.YTop);
             GameObject ObjAsteroid = Instantiate(_arrayprefubAsteroid[Random.Range(0, _arrayprefubAsteroid.Count - 1)], _startPosition, _startRotation);
 
             _attachedScriptsToObj.Add(ObjAsteroid, ObjAsteroid.GetComponent<Asteroid>());
@@ -57,7 +59,7 @@ public class SpawnAsteroid : MonoBehaviour
     public void Respawn(GameObject Asteroid)
     {
         Asteroid.GetComponent<SpriteRenderer>().sprite = _arrayprefubAsteroid[Random.Range(0, _arrayprefubAsteroid.Count - 1)].GetComponent<SpriteRenderer>().sprite;
-        _startPosition.y = Random.Range(ValueExtremePoints.YBot, ValueExtremePoints.YTop);
+        _startPosition.y = Random.Range(_valueExtremePoints.YBot, _valueExtremePoints.YTop);
         Asteroid.transform.position = _startPosition;
     }
 
